38f7e4259cc4dae8746832fbd24f658c096da321
Nikolas <129743960+NikolasProg@users.noreply.github.com>
2023-12-28 07:21:08 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -119,9 +119,10 @@ let products = [
 let listCards = {};
 
 // Добавьте следующий код после вашего существующего кода создания продуктов
-products.forEach((product) => {
+products.forEach((product, index) => {
     let newDiv = document.createElement('div');
     newDiv.classList.add('item');
+    newDiv.setAttribute('data-key', product.id); // Добавляем атрибут data-key
     newDiv.innerHTML = `
         <img src="image/${product.image}" data-description="${product.description}" class="picture">
         <div class="title">${product.name}</div>
@@ -179,6 +180,65 @@ function getFormattedPrice(price) {
     return price !== 'такого объёма нет' ? String(price).replace(/р$/, '') + 'р' : 'такого объёма нет';
 }
 
+// Анимация
+function addToCartAnimation(productKey) {
+    const item = document.querySelector(`.item[data-key="${productKey}"]`);
+    const img = item.querySelector('img');
+
+    // Дублируем картинку
+    const clone = img.cloneNode(true);
+
+    // Применяем стили к клону
+    clone.style.opacity = '0.8';
+    clone.style.position = 'absolute';
+    clone.style.height = '150px';
+    clone.style.width = '150px';
+    clone.style.objectFit = 'cover';
+    clone.style.zIndex = '100';
+    clone.style.transition = 'all 1s ease-in-out';
+
+    // Устанавливаем начальные координаты клонированной картинки
+    clone.style.top = `${img.offsetTop}px`;
+    clone.style.left = `${img.offsetLeft}px`;
+
+    document.body.appendChild(clone);
+
+    const shoppingCart = document.querySelector('.shopping');
+    const cartRect = shoppingCart.getBoundingClientRect();
+
+    // Задаем координаты, куда должен двигаться клон
+    clone.style.top = `${cartRect.top + 5}px`;
+    clone.style.left = `${cartRect.left + 5}px`;
+    clone.style.width = '75px';
+    clone.style.height = '75px';
+
+    // Событие, которое происходит после завершения анимации
+    clone.addEventListener('transitionend', () => {
+        document.body.removeChild(clone);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Получите все элементы с классом 'item'
+    let items = document.querySelectorAll('.item');
+
+    // Добавьте обработчик события для каждого элемента
+    items.forEach(function (item) {
+        let img = item.querySelector('img');
+
+        // Добавьте обработчик события для клика на изображение
+        img.addEventListener('click', function () {
+            // Получите data-key из атрибута 'data-key'
+            let productKey = item.getAttribute('data-key');
+
+            // Вызов функции для анимации добавления в корзину
+            addToCartAnimation(productKey);
+        });
+    });
+});
+
+
+
 function addToCard(key) {
     const selectedVolume = document.getElementById(`user_obym_${key}`).value;
     const productKey = `${key}_${selectedVolume}`;
@@ -200,6 +260,8 @@ function addToCard(key) {
         }
 
         reloadCard();
+		// Вызов функции для анимации добавления в корзину
+        addToCartAnimation(productKey);
     } else {
         alert('Такого объёма нет. Выберите другой объем.');
     }
@@ -330,4 +392,20 @@ overlay.addEventListener('click', () => {
         overlay.style.display = 'none';
         form.style.display = 'none';
     }, 300);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Получите все элементы с классом 'item'
+    let items = document.querySelectorAll('.item');
+
+    // Добавьте обработчик события для каждого элемента
+    items.forEach(function (item, index) {
+        let button = item.querySelector('button');
+
+        // Добавьте обработчик события для клика на кнопку "Добавить в корзину"
+        button.addEventListener('click', function () {
+            // Вызов функции для анимации добавления в корзину
+            addToCartAnimation(index + 1); // Индекс + 1 соответствует ID продукта
+        });
+    });
 });
